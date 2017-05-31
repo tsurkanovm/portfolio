9fe6971b635ee07d9c6fe19534d7abb1a54dc82e
Tsurkanov Mikhail <tsurkanovm@gmail.com>
2017-05-31 17:20:15 +0300

    add project admin actions

--- src/AppBundle/Controller/Admin/ProjectController.php
+++ src/AppBundle/Controller/Admin/ProjectController.php
@@ -9,13 +9,40 @@ use Symfony\Component\HttpFoundation\Request;
 class ProjectController extends Controller
 {
     /**
-     * @Route("/projects", name="projects_list")
+     * @Route("/projects", name="admin_project_list")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
+    {
+        $projects = $this->getDoctrine()
+            ->getRepository('AppBundle:Project')
+            ->findAll();
+
+        return $this->render('admin/project/index.html.twig', [
+            'projects' => $projects,
+        ]);
+    }
+
+    /**
+     * @Route("/new_project", name="admin_project_add")
+     */
+    public function addAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+
+     /**
+     * @Route("/edit_project", name="admin_project_edit")
+     */
+    public function editAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+
 }
