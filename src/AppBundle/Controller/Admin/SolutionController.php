f4e593fea9a1b762ad5d6672bafb9e3429ea9ae0
Tsurkanov Mikhail <tsurkanovm@gmail.com>
2017-07-05 11:51:53 +0300

    add Solution actions and templates

--- src/AppBundle/Controller/Admin/SolutionController.php
+++ src/AppBundle/Controller/Admin/SolutionController.php
@@ -2,8 +2,11 @@
 
 namespace AppBundle\Controller\Admin;
 
+use AppBundle\Entity\Solution;
+use AppBundle\Form\SolutionFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class SolutionController extends Controller
 {
@@ -12,13 +15,46 @@ class SolutionController extends Controller
      */
     public function indexAction()
     {
-//        $projects = $this->getDoctrine()
-//            ->getRepository('AppBundle:Project')
-//            ->findAll();
-//
-        return $this->render('admin/project/index.html.twig', [
-            'projects' => [],
+        $solutions = $this->getDoctrine()
+            ->getRepository('AppBundle:Solution')
+            ->findAll();
+
+        return $this->render('admin/solution/index.html.twig', [
+            'solutions' => $solutions,
         ]);
 
+    }
+
+    /**
+     * @Route("/new_solution", name="admin_solution_add")
+     */
+    public function addAction(Request $request)
+    {
+        $form = $this->createForm(SolutionFormType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $solution = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($solution);
+            $em->flush();
+        }
+
+        return $this->render('admin/solution/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/edit_solution/{id}", name="admin_solution_edit")
+     */
+    public function editAction(Solution $solution)
+    {
+        $form = $this->createForm(SolutionFormType::class);
+        $form->setData($solution);
+        return $this->render('admin/solution/edit.html.twig', [
+            'solution' => $solution,
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/AppBundle/Form/SolutionFormType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SolutionFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\Solution'
+        ]);
+    }
+}
