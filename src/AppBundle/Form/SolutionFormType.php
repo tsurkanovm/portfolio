8d84ef25c3284825b40c33487363d5f100bd316b
Tsurkanov Mikhail <tsurkanovm@gmail.com>
2017-07-26 18:55:27 +0300

    add file storage entity

--- src/AppBundle/Form/SolutionFormType.php
+++ src/AppBundle/Form/SolutionFormType.php
@@ -5,7 +5,6 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class SolutionFormType extends AbstractType
 {
@@ -13,15 +12,7 @@ class SolutionFormType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('imageFile', VichImageType::class, [
-                'required' => true,
-                'allow_delete' => false,
-                'label' => 'Solution image',
-                'download_uri' => false,
-                'download_label' => 'Down',
-                'image_uri' => true,
-//                'imagine_pattern' => '...',
-            ]);
+            ->add('logo');
     }
 
     public function configureOptions(OptionsResolver $resolver)
---
+++ src/AppBundle/Repository/FileStorageRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * FileStorageRepository
+ *
+ * @author Tsurkanov Mihail <tsurkanovm@gmail.com>
+ */
+class FileStorageRepository extends EntityRepository
+{
+
+}
