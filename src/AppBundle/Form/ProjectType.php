b647965ceea65d71ef2c19d481aae19acc462bdf
Tsurkanov Mikhail <tsurkanovm@gmail.com>
2017-08-08 10:59:22 +0300

    add to solution item choice to project

--- src/AppBundle/Form/ProjectType.php
+++ src/AppBundle/Form/ProjectType.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\Solution;
 use AppBundle\Manager\FileStorageManager;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -56,6 +58,11 @@ class ProjectType extends AbstractType
                     'Enabled' => true,
                     'Disabled' => false,
                 ]
+            ])
+            ->add('solutions', EntityType::class, [
+                'class' => Solution::class,
+                'multiple' => true,
+                'attr' => array('class' => 'selectpicker'),
             ]);
     }
 
